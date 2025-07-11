621d46ec084cfb84252b2664d9aa1407aa76ad0b
dj16900 <38293716+dj16900@users.noreply.github.com>
2025-07-11 21:17:05 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Mr. Debjit Ghosh',
+  title: 'Debjit Ghosh',
   description: 'Created by Debjit Ghosh',
   authors: [{ name: 'Debjit Ghosh' }],
 }
